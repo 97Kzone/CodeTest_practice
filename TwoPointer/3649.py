c967e978f2b14870ad5b8a66d93ddce3c28b05ae
97Kzone <76652908+97Kzone@users.noreply.github.com>
2022-08-18 00:51:22 +0900

    22.08.18 투 포인터 1 solve

---
+++ TwoPointer/3649.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    try:
+        target = int(input()) * (10 ** 7)
+        N = int(input())
+        b = [int(input()) for _ in range(N)]
+        b.sort()
+
+        i, j = 0, N-1
+        while j < N and i < j:
+            if b[i] + b[j] == target:
+                print("yes " + str(b[i]) + " " + str(b[j]))
+                break
+
+            elif b[i] + b[j] < target:
+                i += 1
+            else:
+                j -= 1
+
+        else:
+            print("danger")
+    except:
+        exit()
