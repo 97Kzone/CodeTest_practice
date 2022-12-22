81f52f9c003e7d0a426980a3321e36a2800d4f71
97Kzone <76652908+97Kzone@users.noreply.github.com>
2022-12-22 22:17:46 +0900

    22.12.22 백준 이분탐색 1 solve

---
+++ BinarySearch/23631.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+def check():
+    l, r = 0, 10**6
+
+    while l < r:
+        m = (l+r)//2
+
+        if m*(m+1)*K >= 2*N:
+            r = m
+        else:
+            l = m+1
+        
+    return l
+
+for _ in range(int(input())):
+    N, K = map(int, input().split())
+
+    v = check()
+    std = v*(v+1)//2
+
+    if v%2 != 0:
+        res = K*((v//2)+1)
+        res += (N-1)-std*K
+        print(str(res) + " R")
+    else:
+        res = -K*(v//2)
+        res-=(N-1)-std*K
+        print(str(res) + " L")
