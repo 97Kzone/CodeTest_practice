54f47116c8c86aad19d4336c5694c1bc1c5774ee
97Kzone <76652908+97Kzone@users.noreply.github.com>
2022-12-26 22:50:35 +0900

    22.12.26 백준 1 solve

---
+++ TwoPointer/25916.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+
+res = 0
+i, j, t = 0, 1, 0
+
+if N == 1:
+    print(nums[0] if nums[0] <= M else 0)
+    exit()
+
+if nums[i] <= M:
+    t += nums[i]
+    
+while i <= j and j < N:
+    tmp = t + nums[j]
+    if tmp <= M:
+        t += nums[j]
+        j += 1
+    else:
+        if nums[j] > M and j < N-1:
+            i = j+1
+            j += 1
+            t = 0
+        else:
+            t -= nums[j]
+            i += 1
+    
+    res = max(res, t)
+        
+print(res)
