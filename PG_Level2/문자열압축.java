52026fe3cb37f6f58d431b3d7edeb6bad1a430b9
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-03-23 21:55:26 +0900

    23.03.23 SSAFY 스터디 풀이 수정

--- PG_Level2/문자열압축.java
+++ PG_Level2/문자열압축.java
@@ -1,12 +1,15 @@
 package PG_Level2;
 
+import java.lang.Math;
+
 /*
  * 접근법
  * 1. 문자열 s 의 최대길이 1000, 완전탐색(N^2) 100만 <= 1초
  * 2. 완전탐색으로 그냥 되는 길이 다 잘라본다
  * 3. BUT, 압축하려면 최소 "2등분" 되어야 의미 있으니 유의하자
  */
-public class 문자열압축 {
+
+class Solution {
     static int res, size;
     static String input;
 
@@ -27,26 +30,34 @@ public class 문자열압축 {
     }
 
     static void check(int l) {
-        int cnt = 0;
+        int cnt = 1;
         StringBuilder sb = new StringBuilder();
 
+        int e;
         String tmp = input.substring(0, l);
-        for (int i = 0; i < size; i++) {
-            if (tmp.equals(input.substring(i, i + l)))
+        for (int i = l; i < size; i += l) {
+            e = i + l >= size ? size : i + l;
+
+            if (tmp.equals(input.substring(i, e)))
                 cnt++;
             else {
+
                 if (cnt < 2)
                     sb.append(tmp);
                 else
                     sb.append(cnt + tmp);
-                tmp = input.substring(i, i + l);
+
+                tmp = input.substring(i, e);
                 cnt = 1;
             }
         }
 
         // 잔반처리
-        if (cnt != 1)
+        if (cnt < 2)
+            sb.append(tmp);
+        else
             sb.append(cnt + tmp);
+
         res = Math.min(res, sb.length());
     }
 }
