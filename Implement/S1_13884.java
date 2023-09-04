a4e5e822c7ed7162741f0df86af453d4c0e86465
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-09-04 22:08:42 +0900

    23.09.04 BOJ S1 1 solve

---
+++ Implement/S1_13884.java
@@ -0,0 +1,47 @@
+package Implement;
+
+import java.util.Scanner;
+
+public class S1_13884 {
+    static int K, N;
+    static int[] a;
+    public static void test() {
+        Scanner sc = new Scanner(System.in);
+        K = sc.nextInt();
+        N = sc.nextInt();
+        a = new int[N + 1];
+        
+        for (int i = 1; i <= N; i++)
+            a[i] = sc.nextInt();
+
+        a[0] = 2000000000;
+        int h = 0;
+        int i, m;
+        for (i = 0; i < N; i++) {
+            m = 0;
+            for (int j = N; j >= 1; j--) {
+                if (a[j] < a[m])
+                    m = j;
+                else if (a[j] == a[m]) {
+                    if (j > h)
+                        m = j;
+                }
+            }
+            if (m < h)
+                break;
+            h = m;
+            a[m] = 2000000000;
+        }
+        System.out.println(K + " " + (N - i));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            test();
+        }
+        sc.close();
+    }
+}
+
