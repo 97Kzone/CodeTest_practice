b64b743cf36cff156b419817c52a18df5d5eb87f
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-10-15 11:14:48 +0900

    23.10.15 BOJ S5 1 solve

---
+++ Implement/S5_2628.java
@@ -0,0 +1,12 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S5_2628 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
