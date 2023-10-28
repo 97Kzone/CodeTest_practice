4a15ff443fca8ec0fc601f014026f214ace3f8c7
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-10-28 21:53:51 +0900

    23.10.28 BOJ S5 1 solve

---
+++ Implement/S5_30010.java
@@ -0,0 +1,20 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S5_30010 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        sb.append(N + " ");
+
+        for (int i = 1; i < N; i++) {
+            sb.append(i + " ");
+        }
+        System.out.println(sb.toString());
+    }
+}
