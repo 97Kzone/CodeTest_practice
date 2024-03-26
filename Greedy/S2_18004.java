52b4d168d86bcd9555bccfc0954b8da1e09b275a
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-03-26 21:00:02 +0900

    24.03.26 BOJ S2 Greedy 1 sovle

---
+++ Greedy/S2_18004.java
@@ -0,0 +1,41 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S2_18004 {
+
+    static int a, b;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        // 1. a == b 
+        if (a == b) {
+            System.out.print(res);
+            return;
+        }
+        
+        // 2. a < b
+        if (a < b) {
+            System.out.print(b - a);
+            return;
+        }
+        
+        // 3. a > b
+        int res = 0;
+        while (a > b) {
+            if (a % 2 == 1) a += 1;
+            else a /= 2; 
+            res++;
+        }
+        
+        res += b - a;
+        System.out.print(res);
+    }
+}
