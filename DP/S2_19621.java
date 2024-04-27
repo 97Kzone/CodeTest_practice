bfbcf122eb126337e31ba90b1ee11cc94fcf3697
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-04-27 23:59:55 +0900

    24.04.27 BOJ S2 DP 1 fail

---
+++ DP/S2_19621.java
@@ -0,0 +1,48 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class S2_19621 {
+
+    static int N;
+    static List<int[]> list;
+    static int[] dp;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        dp = new int[Math.max(2, N)];
+        list = new ArrayList<>();
+
+        int s, e, v;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            s = Integer.parseInt(st.nextToken());
+            e = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+
+            list.add(new int[] { s, e, v });
+        }
+
+        dp[0] = list.get(0)[2];
+        dp[1] = Math.max(dp[0], list.get(1)[2]);
+
+        if (N > 1) {
+            for (int i = 2; i < dp.length; i++) {
+                dp[i] = Math.max(dp[i - 1], dp[i - 2] + list.get(i)[2]);
+            }
+        }
+
+        System.out.print(dp[N - 1]);
+    }
+}
