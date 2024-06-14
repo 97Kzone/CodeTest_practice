41d898f9e9bf1e33e000b8699770eda9e7394e94
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-06-14 23:41:19 +0900

    24.06.14 BOJ S5 Implement 1 solve

---
+++ Implement/S5_15688.java
@@ -0,0 +1,26 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class S5_15688 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        } 
+
+        Arrays.sort(nums);
+        for (int n : nums) {
+            sb.append(n).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
