767f6e51fc19d3b58699d4b6661671fc77949002
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-07-05 23:35:04 +0900

    24.07.05 BOJ S5 Implement 1 solve

---
+++ Implement/S4_20186.java
@@ -0,0 +1,33 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class S4_20186 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        int res = 0;
+        for (int i = 1; i <= K; i++) {
+            res += nums[N - i] - i + 1;
+        }
+
+        System.out.print(res);
+    }
+}
