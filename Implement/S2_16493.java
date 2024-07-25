fe88b88029dc9bb2640e8cc96f3dbe2870a24453
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-07-25 23:27:47 +0900

    24.07.25 BOJ S2 Implement 1 solve

---
+++ Implement/S2_16493.java
@@ -0,0 +1,36 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class S2_16493 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] pages = new int[M][2];
+        int[] dp = new int[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            pages[i][0] = Integer.parseInt(st.nextToken());
+            pages[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            for (int j = N; j >= pages[i][0]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - pages[i][0]] + pages[i][1]);
+            }
+        }
+
+        System.out.print(dp[N]);
+    }
+}
