c5c993396cf246211843ba62a91ffeaa4d0cf74d
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-09-03 22:43:48 +0900

    24.09.03 BOJ S2 DP Implement 1 solve

---
+++ BFS/S2_28450.java
@@ -0,0 +1,53 @@
+package BFS;
+
+import java.util.*;
+import java.io.*;
+
+public class S2_28450 {
+
+    static int N, M, H;
+    static int[][] board;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        init();
+
+        H = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                board[i][j] += Math.min(board[i][j - 1], board[i - 1][j]);
+            }
+        }
+
+        if (board[N][M] > H) {
+            System.out.print("NO");
+        } else {
+            System.out.println("YES");
+            System.out.print(board[N][M]);
+        }
+    }
+    
+    static void init() {
+        for (int i = 1; i <= N; i++) {
+            board[i][1] += board[i - 1][1];
+        }
+
+        for (int i = 1; i <= M; i++) {
+            board[1][i] += board[1][i - 1];
+        }
+    }
+}
