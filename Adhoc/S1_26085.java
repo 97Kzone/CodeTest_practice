b6b170b0985dbefb4647d279c349c5d24973b231
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-09-09 23:49:05 +0900

    24.09.09 BOJ S1 Adhoc 1 solve

---
+++ Adhoc/S1_26085.java
@@ -0,0 +1,75 @@
+package Adhoc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S1_26085 {
+
+    static int N, M;
+    static int[][] board;
+    static int[][] moves = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][M];
+
+        int zero = 0;
+        int one = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+
+                if (board[i][j] == 0) {
+                    zero++;
+                } else {
+                    one++;
+                }
+            }
+        }
+
+        if (zero != one) {
+            System.out.print(-1);
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (check(i, j)) {
+                    System.out.print(1);
+                    return;
+                }
+            }
+        }
+
+        System.out.print(-1);
+    }
+    
+    static boolean check(int x, int y) {
+        int nx, ny;
+        for (int[] move : moves) {
+            nx = x + move[0];
+            ny = y + move[1];
+
+            if (!isPossible(nx, ny))
+                continue;
+            
+            if (board[x][y] == board[nx][ny]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static boolean isPossible(int x, int y) {
+        return (0 <= x && x < N) && (0 <= y && y < M);
+    }
+}
