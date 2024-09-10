8c78b59f6e259bc3f0ba619b3306081b0a735935
Kang Dong Pyo <76652908+97Kzone@users.noreply.github.com>
2024-09-10 23:42:11 +0900

    24.09.10 BOJ S5 Implement 1 solve

---
+++ Implement/S5_16212.java
@@ -0,0 +1,34 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class S5_16212 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+        for (int n : list) {
+            sb.append(n + " ");
+        }
+        
+        System.out.print(sb);
+
+
+    }
+}
