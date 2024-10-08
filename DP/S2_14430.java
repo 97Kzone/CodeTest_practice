d4bf2c2d92fa51b0e9949b788eb58ab12c2d1b7f
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-10-08 22:27:46 +0900

    24.10.08 BOJ S2 DP 1 solve

---
+++ DP/S2_14430.java
@@ -0,0 +1,34 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class S2_14430 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] board = new int[N][M];
+        int[][] dp = new int[N+1][M+1];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                dp[i][j] = board[i - 1][j - 1] + Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        System.out.print(dp[N][M]);
+    }
+}
