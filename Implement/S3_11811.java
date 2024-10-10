b2ddb207519f824951065dacb1e3b6e8e37410c5
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-10-10 21:13:29 +0900

    24.10.10 BOJ S3 Implement 1 solve

---
+++ Implement/S3_11811.java
@@ -0,0 +1,40 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class S3_11811 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] board = new int[N][N];
+        int[] res = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j)
+                    continue;
+
+                res[i] = res[i] | board[i][j];
+            }
+        }
+        
+        for (int n : res) {
+            sb.append(n + " ");
+        }
+
+        System.out.print(sb);
+    }
+}
