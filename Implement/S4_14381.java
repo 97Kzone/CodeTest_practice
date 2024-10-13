989ac7391b5eff2c2a0f070563e89168dd8c1b3a
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-10-13 10:37:38 +0900

    24.10.13 BOJ S4 Implement 1 solve

---
+++ Implement/S4_14381.java
@@ -0,0 +1,45 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class S4_14381 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        String[] num;
+        int N, std;
+
+        HashSet<String> set = new HashSet<>();
+        for (int t = 1; t <= T; t++) {
+            N = Integer.parseInt(br.readLine());
+
+            if (N == 0) {
+                sb.append("Case #" + t + ": INSOMNIA" + "\n");
+                continue;
+            }
+
+            set.clear();
+            std = N;
+            for (int i = 1; i <= 1000; i++) {
+                std = N * i;
+                num = String.valueOf(std).split("");
+                for (String n : num) {
+                    set.add(n);
+                }
+
+                if (set.size() == 10) {
+                    break;
+                }
+            }
+
+            sb.append("Case #" + t + ": " + std + "\n");
+        }
+        System.out.print(sb);
+    }
+}
