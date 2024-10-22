12ccbe1ce25018434faf5ccd97337dba824e6b6d
97Kzone <76652908+97Kzone@users.noreply.github.com>
2024-10-22 21:24:45 +0900

    24.10.22 BOJ S3 Greedy 1 solve

---
+++ Greedy/S3_32396.java
@@ -0,0 +1,40 @@
+package Greedy;
+
+import java.util.*;
+import java.io.*;
+
+public class S3_32396 {
+
+    static int N;
+    static long M;
+    static long[] nums;
+    static boolean[] visit;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Long.parseLong(st.nextToken());
+
+        nums = new long[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        visit = new boolean[N];
+        int cnt = 0;
+
+        long std;
+        for (int i = 1; i < N; i++) {
+            std = Math.abs(nums[i] - nums[i - 1]);
+            if (!visit[i - 1] && std < M) {
+                visit[i] = true;
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt);
+
+    }
+}
