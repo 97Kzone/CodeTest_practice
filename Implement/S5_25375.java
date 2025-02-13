0a971498e24851ea0919b7684ceedaf2e0c4ac69
97Kzone <76652908+97Kzone@users.noreply.github.com>
2025-02-14 00:11:51 +0900

    25.02.14 BOJ S5 Implement 1 solve

---
+++ Implement/S5_25375.java
@@ -0,0 +1,32 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S5_25375 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int Q = Integer.parseInt(br.readLine());
+
+        int a, b;
+        for (int i = 0 ; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            if (b % a == 0 && b / a != 1) {
+                sb.append(1 + "\n");
+            } else{
+                sb.append(0 + "\n");
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+}
