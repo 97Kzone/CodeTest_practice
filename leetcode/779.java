a6bc8ea58e8144934b9129cfdf123a78cecffb3a
97Kzone <76652908+97Kzone@users.noreply.github.com>
2026-01-27 22:31:17 +0900

    26.01.27 leetCode 1 solve

---
+++ leetcode/779.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if (n == 1) return 0;
+
+        int res = kthGrammar(n - 1, (k + 1) / 2);
+
+        if (k % 2 == 1) return res;
+        else return res ^ 1; 
+    }
+}
