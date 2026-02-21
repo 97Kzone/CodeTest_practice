dc18fe7eaed2fc3574f77d66cb68f40519c6ac18
97Kzone <76652908+97Kzone@users.noreply.github.com>
2026-02-21 22:23:46 +0900

    26.02.21 leetcode 1 solve

---
+++ leetcode/204.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.*;
+
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        int res = 0;
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) res++;
+        }
+
+        return res;
+    }
+}
